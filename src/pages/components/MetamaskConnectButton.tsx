af65812a1cd666ce0b1ca948b33c3eea94cfc4dc
Olokun <olokungaming@gmail.com>
2023-01-22 13:19:40 -0600

    this works better for intellisense as opposed to abc...

--- src/pages/components/MetamaskConnectButton.tsx
+++ src/pages/components/MetamaskConnectButton.tsx
@@ -13,9 +13,9 @@ import Link from "next/link";
 
 /**
  * Component that renders:
- * a. a button that connects to Metamask
- * b. a button with the current address when connected and disconnects when clicked
- * c. a button that tells the user to install Metamask and opens new tab to Metamask website when clicked
+ * - a button that connects to Metamask
+ * - a button with the current address when connected and disconnects when clicked
+ * - a button that tells the user to install Metamask and opens new tab to Metamask website when clicked
  */
 const MetamaskConnectButton: React.FC<{
   style?: React.CSSProperties;
