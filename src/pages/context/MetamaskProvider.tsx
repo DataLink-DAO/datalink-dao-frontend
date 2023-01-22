6b98e3448dae21d2633bef7cdb75d4a680a063c4
Olokun <olokungaming@gmail.com>
2023-01-22 13:17:28 -0600

    should never be undefined if values are defined

--- src/pages/context/MetamaskProvider.tsx
+++ src/pages/context/MetamaskProvider.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React from "react";
 
 type MetamaskMaskProviderValues = {
-  address: string | null | undefined;
+  address: string | null;
   setAddress: () => void;
   disconnect: () => void;
 };
