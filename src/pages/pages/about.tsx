b030cbd6781435c0f5c2063f8bd0d5b559d0c93c
ShayaanAkbar <shayaanakbar1106@gmail.com>
2023-01-23 19:32:03 -0500

    Added footer, navbar, additional pages and added all the verbiage, need the images + styling now

---
+++ src/pages/pages/about.tsx
@@ -0,0 +1,23 @@
+
+
+const about = () => {
+    return (
+        <div className="about">
+            <div className="text">
+                <h2>
+                    The DataLink DAO will monetize anonymous data-sharing, and provide a powerful tokenization platform for credentials, documents, and other data. 
+
+                    Our system will incentivize the validated data the user is receiving via smart contracts and oracle networks.
+
+                    Our services will give ownership of personal data back to the user while retaining the integrity of that data.
+                </h2>
+            </div>
+
+            <div className="image">
+                <h2>Image goes here</h2>
+            </div>
+        </div>
+    )
+}
+
+export default about;
---
+++ src/pages/pages/products.tsx
@@ -0,0 +1,28 @@
+
+
+const Products = () => {
+    return (
+        <div>   
+            <div className="DataDAO">
+            <h1>DataLink DAO</h1>
+            <h2> The DataLink DAO will monetize anonymous data-sharing, and provide a powerful
+                tokenazation platform for credentials, documents, and other data
+            </h2>
+            </div>
+            <div className="Dashboard">
+                <h1>DataLink Dashboard</h1>
+                <h2>The DAO will host the DataLink Dashboard, a powerful easy-to-use
+                    tokenazation platform for institutions and users
+                </h2>
+            </div>
+            <div className="Validator">
+                <h1>Datalink Validator</h1>
+                <h2>The DataLink validator will use multiple technologies on the forefront
+                    of cryptography research that will aid validation of DAO data, documents, and identities
+                </h2>
+            </div>
+        </div>
+    )
+}
+
+export default Products
