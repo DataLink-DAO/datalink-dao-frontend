1b4b6ab27d6dec80ddaffdad14d41863edbd90d2
electrone901 <carbajalluis@rocketmail.com>
2023-01-31 10:56:29 -0500

    add ethers, publisher and createNFT page

---
+++ src/pages/publisher/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Container } from "@chakra-ui/react";
+import Navbar from "@/components/Navbar";
+import PublisherNft from "@/components/PublisherNft";
+
+const publisher = () => {
+  return (
+    <>
+      <Navbar />
+      <PublisherNft />
+    </>
+  );
+};
+
+export default publisher;
